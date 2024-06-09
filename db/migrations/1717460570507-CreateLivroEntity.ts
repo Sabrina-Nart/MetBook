7f863eae178a661ea1a853c39f13ef9e9ca5dd2a
Sabrina-Nart <sabrinacominnart@gmail.com>
2024-06-08 23:16:39 -0300

    livro

--- db/migrations/1717460570507-CreateLivroEntity.ts
+++ db/migrations/1717460570507-CreateLivroEntity.ts
@@ -1,14 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateLivroEntity1717460570507 implements MigrationInterface {
-    name = 'CreateLivroEntity1717460570507'
+    name = 'CreateLivroEntity1717460570507';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "livros" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "nome" character varying(100) NOT NULL, CONSTRAINT "PK_69daba516e6b0dd45f49c4d8d52" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`
+            CREATE TABLE "livros" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "titulo" character varying NOT NULL,
+                "subtitulo" character varying,
+                "numeroPaginas" integer NOT NULL,
+                "isbn" character varying NOT NULL,
+                "editora" character varying NOT NULL,
+                "anoLancamento" integer NOT NULL,
+                "logoUrl" character varying,
+                "preco" numeric(10,2) NOT NULL,
+                CONSTRAINT "PK_69daba516e6b0dd45f49c4d8d52" PRIMARY KEY ("id")
+            )
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "livros"`);
     }
-
 }
