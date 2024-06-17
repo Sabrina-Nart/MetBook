2e65f612bd606d8da30c709ca0dc47aab2f5de29
Sabrina-Nart <sabrinacominnart@gmail.com>
2024-06-17 19:37:06 -0300

    s

--- db/data-source-seed.d.ts
+++
@@ -1,4 +0,0 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-export declare const dataSourceOptionsSeed: DataSourceOptions;
-declare const _default: DataSource;
-export default _default;
